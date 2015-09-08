ea2da6b432682c3bcf035cb68cd98d39c84f13e8
Hemal Prajapati <hemalprajapati@gmail.com>
2015-09-08 14:56:36 +0530

    Testing Changes

--- src/main/java/com/netweb/annotation/ManageUser.java
+++ src/main/java/com/netweb/annotation/ManageUser.java
@@ -10,8 +10,8 @@ public class ManageUser {
 		/* Add few employee records in database */
 		Integer empID1 = ME.addEmployee("Zara", "Ali", 1000);
 		Integer empID2 = ME.addEmployee("Daisy", "Das", 5000);
-		Integer empID3 = ME.addEmployee("John", "Paul", 10000);
-
+		Integer empID3 = ME.addEmployee("John", "Paul1", 10000);
+ 
 		/* List down all the employees */
 		ME.listEmployees();
 
